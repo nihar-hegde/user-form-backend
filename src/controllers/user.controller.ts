b6fc719b5f5e0c09b0b77c2a641297ffcb211441
Nihar Hegde <niharhegde163@gmail.com>
2024-02-11 16:19:40 +0530

    sending mail done

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { db } from "../database";
 import { z } from "zod";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
 const userFormSchema = z.object({
   name: z.string(),
@@ -11,7 +14,6 @@ const userFormSchema = z.object({
 
 export const createUserForm = async (req: Request, res: Response) => {
   try {
-    console.log(req.body, "REQ body");
     const validatedData = userFormSchema.safeParse(req.body);
     if (!validatedData.success) {
       console.log(validatedData.error);
@@ -25,6 +27,44 @@ export const createUserForm = async (req: Request, res: Response) => {
           email: email,
           phoneNumber: phoneNumber,
         },
+      });
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "noobcoding12@gmail.com",
+          pass: process.env.APP_PASSWORD,
+        },
+      });
+
+      const dob = new Date(createUserForm.dateOfBirth);
+
+      const options: Intl.DateTimeFormatOptions = {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+      };
+
+      const formatter = new Intl.DateTimeFormat("en-US", options);
+      const formattedDate = formatter.format(dob);
+      const mailOptions = {
+        from: {
+          name: "User Form",
+          address: process.env.USER!,
+        },
+        to: createUserForm.email,
+        subject: "Your Form Data",
+        text: `Hi ${createUserForm.name},\n\nHere's a summary of the form data you submitted:\n\nName: ${createUserForm.name}\nDate of Birth: ${formattedDate}\nEmail: ${createUserForm.email}\nPhone Number: ${createUserForm.phoneNumber}\n\nThank you for submitting the form.`,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log("Email sent:", info.response);
+        }
       });
       res.status(200).json({ message: "use created", data: createUserForm });
     }
